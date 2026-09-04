aa6de8dbedaff5728c576ba60fed6c52aa01a6e1
loryujc <abcqwerty775@mail.ru>
2026-09-05 03:24:41 +0500

    Task6

---
+++ Tyuiu.ErmakovSE.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ErmakovSE.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ErmakovSE.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e8388d4f-5c1c-4449-a9ac-459860bc96e5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
