3422eb83b05c2df11af44adf6dfc0cfc36623317
loryujc <abcqwerty775@mail.ru>
2026-09-04 20:42:14 +0500

    task1

---
+++ tyuiu.ermakovse.sprint0.task1.v0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tyuiu.ermakovse.sprint0.task1.v0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Сава";
+            int age = 45;
+            Console.WriteLine("Здравсствуйте, меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
