28b0990904e46b4fa7340f9e8e20032ff84dd157
savel <savel@LORYUJC>
2026-09-04 20:16:48 +0500

    Add project files.

---
+++ tyuiu.ermakovse.sprint0.task0.v0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tyuiu.ermakovse.sprint0.task0.v0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
